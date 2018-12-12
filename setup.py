59bc403bc223eb0b677121d8198e3926fdbd6935
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-12-12 09:55:08 +0100

    pushed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info.major < 3:
 
 setup(
     name="mcgridprep",
-    version="0.4.0",
+    version="0.4.1",
     description="Prepare/run 2D grids using OpenMolcas.",
     url="https://github.com/eljost/mcgridprep",
     maintainer="Johannes Steinmetzer",
